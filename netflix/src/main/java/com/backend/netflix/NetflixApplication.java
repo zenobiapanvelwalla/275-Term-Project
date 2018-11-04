6c64de105db441be35acb4baece94efb4e7c9b79
zenobia panvelwalla <zenobiapanvelwalla@gmail.com>
2018-11-03 20:01:19 -0700

    Spring Boot Initializer - REST API's

---
+++ netflix/src/main/java/com/backend/netflix/NetflixApplication.java
@@ -0,0 +1,12 @@
+package com.backend.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixApplication.class, args);
+	}
+}
