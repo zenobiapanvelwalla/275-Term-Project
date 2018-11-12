65179774305805fda6299b87c02caf23652604ce
ArshiyaPathan <arshiyapathan@gmail.com>
2018-11-11 19:17:13 -0800

    fixing error

--- netflix/src/main/java/com/backend/netflix/repository/BillingStatusRepository.java
+++ netflix/src/main/java/com/backend/netflix/repository/BillingStatusRepository.java
@@ -1,8 +1,9 @@
 package com.backend.netflix.repository;
 
+import com.backend.netflix.vo.BillingStatus;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BillingStatusRepository extends CrudRepository<T, ID> {
+public interface BillingStatusRepository extends CrudRepository<BillingStatus, Long> {
 	
 	
 
