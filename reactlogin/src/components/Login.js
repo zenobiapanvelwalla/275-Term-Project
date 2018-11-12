23f557874d60010601b273c5eb0f8204d2e71534
KiratiBhuva <35611972+KiratiBhuva@users.noreply.github.com>
2018-11-11 20:21:59 -0800

    sign in/ sign up page

--- reactlogin/src/components/Login.js
+++ reactlogin/src/components/Login.js
@@ -1,25 +1,62 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import login from "./../custom_css/login.css";
+import {withRouter} from 'react-router-dom';
 import NavBar from './NavBar';
+import login from "../custom_css/login.css";
 
 
 class Login extends Component {
 
-    static propTypes = {
-        handleSubmit: PropTypes.func.isRequired
-    };
+    state={
+        userdata: {
+            emailorusername: '',
+            password: '',
+        },
+        emailorusernameValid: true,
+        passwordValid: true,
+        msg:false
+    }
 
-    state = {
-        email: '',
-        password: ''
-    };
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    componentWillMount(){
-        this.setState({
-            email: '',
-            password: ''
-        });
+    handleSubmit(e){
+        e.preventDefault();
+        console.log("Call to page ");
+        if(this.validateUsername() === true)
+        {
+            if(this.validatePassword() === true)
+            {
+                
+            }
+            else
+            {
+                this.setState({passwordValid: false})
+            }
+        }
+        else
+        {
+            //this.state.usernameValid = false;
+            this.setState({emailorusernameValid: false})
+        }
+    }
+
+    validateUsername() {
+        var username = this.refs.email.value;
+        if (username !== '')
+        {
+            return (true)
+        }
+        return (false)
+    }
+    validatePassword(){
+        var password = this.refs.password.value;
+        if (password !== '')
+        {
+            return (true)
+        }
+        return (false)
     }
 
     render() {
@@ -38,32 +75,51 @@ class Login extends Component {
                                 <span><i className="fab fa-twitter-square"></i></span>
                             </div>
                         </div>
+                        <div >
+                            {this.props.loginMsg && (
+                                <div className="alert alert-warning text-danger small" role="alert">
+                                    {this.props.loginMsg}
+                                </div>
+                            )}
+                        </div>
                         <div className="card-body">
-                            <form>
                                 <div className="input-group form-group">
                                     <div className="input-group-prepend">
                                         <span className="input-group-text"><i className="fas fa-user"></i></span>
                                     </div>
-                                    <input type="text" name = "email" className="form-control" placeholder="email"/>
+                                    { this.state.emailorusernameValid ? null : <div className="text-input-error-wrapper text-left errormessage">Username is required.</div>}
+                                    <input type="text" name = "email" className="form-control" placeholder="email"
+                                           ref="email"
+                                           onFocus={(event) => {
+                                           this.setState({emailorusernameValid: true, msg : false});
+                                    }} />
 
                                 </div>
                                 <div className="input-group form-group">
                                     <div className="input-group-prepend">
                                         <span className="input-group-text"><i className="fas fa-key"></i></span>
                                     </div>
-                                    <input name = "password" type="password" className="form-control" placeholder="password"/>
+                                    { this.state.passwordValid ? null : <div className="text-input-error-wrapper text-left errormessage">Password is required.</div>}
+                                    <input name = "password" type="password" className="form-control" placeholder="password"
+                                           ref = "password"
+                                           onFocus={(event) => {
+                                               this.setState({passwordValid: true , msg: false});
+                                           }}
+                                    />
                                 </div>
                                 <div className="form-group">
-                                    <input type="submit" value="Login" className="btn float-right login_btn" />
+                                    <button name="Login" value="Login" className="btn float-right login_btn"
+                                           onClick={(event) => {
+                                               this.handleSubmit(event)
+                                           }}>Sign In</button>
                                 </div>
-                            </form>
                         </div>
                         <div className="card-footer">
                             <div className="d-flex justify-content-center links">
-                                Don't have an account?<a href="#">Sign Up</a>
+                                Don't have an account?<a href="signup">Sign Up</a>
                             </div>
                             <div className="d-flex justify-content-center">
-                                <a href="#">Forgot your password?</a>
+                                <a href="signup">Forgot your password?</a>
                             </div>
                         </div>
                     </div>
@@ -74,4 +130,4 @@ class Login extends Component {
     }
 }
 
-export default Login;
+export default withRouter(Login);
