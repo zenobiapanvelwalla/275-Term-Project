404ed20ae4f27b3362317f2e46c758e6c5a13e3a
abrahamwilliam <36279092+abrahamwilliam@users.noreply.github.com>
2018-11-11 17:58:40 -0800

    Add files via upload

---
+++ netflix/src/main/java/com/backend/netflix/vo/CustomUserDetail.java
@@ -0,0 +1,61 @@
+package com.backend.netflix.vo;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetail extends User implements UserDetails {
+
+	public CustomUserDetail(final User users) {
+		super(users);
+		
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		System.out.println("Getting roles "+getRoles());
+		
+		return getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRole()))
+                .collect(Collectors.toList());
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return super.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return super.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
